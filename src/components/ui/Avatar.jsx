094b0efe03dbb7e550557651f6337ecdd0154b5e
hayaman3 <kevin.haya@yahoo.com>
2023-06-27 22:16:36 +0800

    clean unused

--- src/components/ui/Avatar.jsx
+++ src/components/ui/Avatar.jsx
@@ -3,7 +3,7 @@ import ContentfulImage from './ContentfulImage'
 const Avatar = ({ name, picture }) => {
   return (
     <div className='flex items-center'>
-      <div className='relative w-10 h-10 mr-4'>
+      <div className='relative w-14 h-10 mr-4'>
         <ContentfulImage
           src={picture.fields.file.url}
           layout='fill'
