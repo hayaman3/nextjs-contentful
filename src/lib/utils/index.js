f6c1ec7c319cbf52b7c2360261dfc27d4fce5159
hayaman3 <kevin.haya@yahoo.com>
2023-06-29 21:34:27 +0800

    fixed bug date

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,5 +1,4 @@
 export function formatDate(dateString  , options ) {
-  // const { format } = new Intl.DateTimeFormat('en-US', options)
-  // return format(new Date(dateString))
-  return "date"
+  const { format } = new Intl.DateTimeFormat('en-US', options)
+  return format(new Date(dateString))
 }
